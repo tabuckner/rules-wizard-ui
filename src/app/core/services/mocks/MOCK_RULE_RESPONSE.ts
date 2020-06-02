75dadb381f95babcabf8932656d9e30a20e2d4a9
Mike Olivas <molivas@datical.com>
2020-06-02 14:49:31 -0500

    Removed extra spaces

--- src/app/core/services/mocks/MOCK_RULE_RESPONSE.ts
+++ src/app/core/services/mocks/MOCK_RULE_RESPONSE.ts
@@ -1,31 +1,31 @@
 import { RuleResponse } from 'src/app/models/rule-response';
 
 export const MOCK_RULE_RESPONSE: RuleResponse = {
-  rule: `// Copyright (c) 2020 Datical, Inc. All Rights Reserved.
-  /*
-  @author Taylor Buckner
-  @version 1.0
-  @date June 2, 2020
-  @description Error if GRANT is detected
-  */
+rule: `// Copyright (c) 2020 Datical, Inc. All Rights Reserved.
+/*
+@author Taylor Buckner
+@version 1.0
+@date June 2, 2020
+@description Error if GRANT is detected
+*/
 
-  package com.datical.hammer.core.sqlrules.java
+package com.datical.hammer.core.sqlrules.java
 
-  import com.datical.db.project.Project;
-  import com.datical.hammer.core.rules.Response;
-  import com.datical.hammer.core.rules.Response.ResponseType;
-  import com.datical.hammer.core.rules.WithComments;
-  import com.datical.hammer.core.rules.WithoutComments;
+import com.datical.db.project.Project;
+import com.datical.hammer.core.rules.Response;
+import com.datical.hammer.core.rules.Response.ResponseType;
+import com.datical.hammer.core.rules.WithComments;
+import com.datical.hammer.core.rules.WithoutComments;
 
-  rule "Error when GRANTs are in SQL Scripts"
-    salience 1
-    when
-      wc : WithComments(getText().toLowerCase() contains "grant" )
-    then
-      String errorMessage =
-        "GRANT found in file " +
-        wc.getSqlFile().getName() + " on lines: " + wc.getLineNumbers("grant");
-      insert(new Response(ResponseType.FAIL, errorMessage, drools.getRule().getName()));
-    end
-  `
+rule "Error when GRANTs are in SQL Scripts"
+  salience 1
+  when
+    wc : WithComments(getText().toLowerCase() contains "grant" )
+  then
+    String errorMessage =
+      "GRANT found in file " +
+      wc.getSqlFile().getName() + " on lines: " + wc.getLineNumbers("grant");
+    insert(new Response(ResponseType.FAIL, errorMessage, drools.getRule().getName()));
+  end
+`
 };
