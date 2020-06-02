9759d5955a68f0be121859e7ad5b3e5051040419
Taylor Buckner <taylora.buckner@gmail.com>
2020-06-02 11:25:46 -0500

    feat(navigation): basic app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WizardComponent } from './wizard/wizard.component';
+import { HomeComponent } from './home/home.component';
+import { ValidatorComponent } from './validator/validator.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'wizard', component: WizardComponent },
+  { path: 'validator', component: ValidatorComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
