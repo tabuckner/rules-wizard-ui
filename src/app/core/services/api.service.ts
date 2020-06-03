ca7f42046bc6589b84ecd571e7724a56d435ed89
Taylor Buckner <taylora.buckner@gmail.com>
2020-06-03 08:52:58 -0500

    fix: refactor mikes code and suppress errors.

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,32 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { RuleDto } from 'src/app/models/rule-dto';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { RuleResponse } from 'src/app/models/rule-response';
-import { MOCK_RULE_RESPONSE } from './mocks/MOCK_RULE_RESPONSE';
-import { tap, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  //private readonly baseUrl = 'https://rules-hackathon.herokuapp.com/api/rule'; // TODO: Get From Ben
+  // private readonly baseUrl = 'https://rules-hackathon.herokuapp.com/api/rule'; // TODO: Get From Ben
   private readonly baseUrl = 'http://localhost:8080/api/rule'; // TODO: Get From Ben
 
   constructor(private http: HttpClient) { }
 
   public createRule(ruleDto: RuleDto): Observable<RuleResponse> {
+    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU5MTIyNTM1Nn0.5kIiehwvUmVJpPoIyZ24Q9yRRu304jr3cFHd8tl1ml816fynJOFTooE-M6HOHpb5DrzgR6gqtLm9KoXxL3-fow';
     const url = `${this.baseUrl}/sqlrule`;
-    let token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU5MTIyNTM1Nn0.5kIiehwvUmVJpPoIyZ24Q9yRRu304jr3cFHd8tl1ml816fynJOFTooE-M6HOHpb5DrzgR6gqtLm9KoXxL3-fow';
-    const fullURL = `${url}?ruleName=${ruleDto.ruleName}&sql=${ruleDto.sql}&message=${ruleDto.message}&level=${ruleDto.level}`;
-    //return of(MOCK_RULE_RESPONSE);
-    return this.http.post<{ rule: string }>(fullURL, {},
-      {
-        headers:
-          { 'Authorization': 'Bearer ' + token },
-          responseType:"text"
-      }).pipe(
-        map(data => ({rule: data}))
-      );
+    const params = new HttpParams({ fromObject: { ...ruleDto } });
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    const options = {
+      params,
+      headers,
+      responseType: 'text' as 'json'
+    };
+
+    // return of(MOCK_RULE_RESPONSE);
+    return this.http.post(url, {}, options).pipe(
+      map(data => ({ rule: data } as RuleResponse))
+    );
   }
 }
